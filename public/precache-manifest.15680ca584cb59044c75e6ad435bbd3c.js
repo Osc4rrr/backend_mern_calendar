1d1628284ab3c47daddee95e93b07a6014a57e16
oscar.diaz <os.diazl@alumnos.duoc.cl>
2021-02-25 19:30:15 -0300

    v1 calendarmern

---
+++ public/precache-manifest.15680ca584cb59044c75e6ad435bbd3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbcf2c2ba678510b427ac136f186ce1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c64d19c4b7749bc18eb",
+    "url": "/static/css/2.1d09e8c2.chunk.css"
+  },
+  {
+    "revision": "b8a6157afb20e2a76ea9",
+    "url": "/static/css/main.d4c051f9.chunk.css"
+  },
+  {
+    "revision": "5c64d19c4b7749bc18eb",
+    "url": "/static/js/2.00e63b19.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/static/js/2.00e63b19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8a6157afb20e2a76ea9",
+    "url": "/static/js/main.0dbad434.chunk.js"
+  },
+  {
+    "revision": "e5f6df699a1b3c2b8af0",
+    "url": "/static/js/runtime-main.f67dedc7.js"
+  }
+]);
